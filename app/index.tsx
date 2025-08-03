bc9a1714b933fa66b026a1e662de5ab58083a623
liberto romero <liberto3dejulio@gmail.com>
2025-08-03 17:50:55 +0200

    estructura base del proyecto con expo-router y carpetas feature

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+// app/index.tsx
+import { Text, View } from 'react-native'
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Bienvenido a App Restaurante</Text>
+    </View>
+  )
+}
